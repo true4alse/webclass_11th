9beabf96d1e06e4ada2cb3d7d8999861d316778d
true4alse <true4alse@naver.com>
2025-09-29 13:13:03 +0900

    0929

---
+++ html-class/ch31-object/js/classmate.js
@@ -0,0 +1,183 @@
+let classmate = [
+    {
+        id:0,
+        name:"정현진",
+        thumb:"정현진.png",
+        age:50,
+        gender:"남자",
+        married:false,
+        mbti:"INFP",
+        hobbies:[
+            "탄천 뛰기",
+            "게임하기",
+            "청소하기",
+            "친구들 만나서 놀기"
+        ]
+    },
+    {
+        id:1,
+        name:"박진솔",
+        thumb:"박진솔.png",
+        age:27,
+        gender:"여자",
+        married:false,
+        mbti:"ESTP",
+        hobbies:[
+            "산책하기",
+            "게임하기",
+            "노래듣기",
+            "친구랑 술 한잔"
+        ]
+    },
+    {
+        id:2,
+        name:"임은경",
+        thumb:"임은경.png",
+        age:30,
+        gender:"여자",
+        married:false,
+        mbti:"INFP",
+        hobbies:[
+            "게임하기",
+            "노래들으며 산책하기",
+        ]
+    },
+    {
+        id:3,
+        name:"유승진",
+        thumb:"유승진.png",
+        age:27,
+        gender:"남자",
+        married:false,
+        mbti:"INFJ",
+        hobbies:[
+            "게임하기",
+            "강아지랑 산책하기",
+        ]
+    },
+    
+     {   id:4,
+        name:"이지하",
+        thumb:"이지하.png",
+        age:27,
+        gender:"여자",
+        married:false,
+        mbti:"INFJ",
+        hobbies:[
+            "노래듣기",
+            "분석하기",
+            "계획짜기",
+        ]
+    },
+    {
+        id:5,
+        name:"이혜림",
+        thumb:"이혜림.png",
+        age:27,
+        gender:"여자",
+        married:false,
+        mbti:"INTJ",
+        hobbies:[
+            "노래듣기",
+            "강아지 산책",
+            "독서",
+            "영화보기"
+        ]
+    },
+    {
+        id:6,
+        name:"김서진",
+        thumb:"김서진.png",
+        age:23,
+        gender:"여자",
+        married:true,
+        mbti:"ISTJ",
+        hobbies:[
+            "쇼핑하기",
+            "옷 정리하기",
+            "유튜브보기",
+            "화장품 정리하기",
+            "맛집투어하기"
+        ]
+    },
+    {
+        id:7,
+        name:"박채은",
+        thumb:"박채은.png",
+        age:23,
+        gender:"여자",
+        married:false,
+        mbti:"ISFP",
+        hobbies:[
+            "게임하기",
+            "노래듣기",
+            "맛집탐방",
+            "까페가기",
+            "영화보기"
+        ]
+    },
+    {
+        id:8,
+        name:"구연서",
+        thumb:"구연서.png",
+        age:27,
+        gender:"여자",
+        married:false,
+        mbti:"INTP",
+        hobbies:[
+            "까페가기",
+            "gpt랑 대화하기",
+            "집에서 맥주마시기",
+            "집에서 누워있기",
+            "집에서 티비보기"
+        ]
+    },
+    {
+        id:9,
+        name:"엄혜경",
+        thumb:"엄혜경.png",
+        age:25,
+        gender:"여자",
+        married:false,
+        mbti:"INTP",
+        hobbies:[
+            "멍때리기",
+            "게임하기",
+            "배너 만들기",
+        ]
+    },
+    {
+        id:10,
+        name:"김민영",
+        thumb:"김민영.png",
+        age:33,
+        gender:"여자",
+        married:false,
+        mbti:"ESTP",
+        hobbies:[
+            "드라이브",
+            "음악감상",
+            "여행",
+            "산책",
+            "단도리",
+            "까페가기"
+        ]
+    },
+    {
+        id:11,
+        name:"김혜빈",
+        thumb:"김혜빈.png",
+        age:27,
+        gender:"여자",
+        married:false,
+        mbti:"INFP",
+        hobbies:[
+            "유튜브 틀어놓고 누워있기",
+            "책 읽기",
+            "녹차라떼 먹기",
+            "와이어프레임만들기",
+        ]
+    },
+]
+
+
